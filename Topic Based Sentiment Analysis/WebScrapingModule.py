4766480e9d49d3b4efae1dafdfa7755d845d5b7c
Deven Biehler <124630485+Deven-Biehler@users.noreply.github.com>
2023-09-22 10:27:46 -0700

    code review 1

---
+++ Topic Based Sentiment Analysis/WebScrapingModule.py
@@ -0,0 +1,8 @@
+import spacy
+from spacytextblob.spacytextblob import SpacyTextBlob
+import pandas as pd
+import requests
+from newspaper import Article
+from collections import Counter
+import re
+
